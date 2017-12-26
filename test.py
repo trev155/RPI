7f91537e651a8bf5c1a00c86f7b43b859b147147
Trevor <trevku155@gmail.com>
2017-12-26 17:22:07 -0500

    c

--- test.py
+++ test.py
@@ -6,12 +6,20 @@ BUTTON_PIN=23
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(BUTTON_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
+isPressed = False
+isOn = False
+
 try:
 	while True:
-		input_state = GPIO.input(BUTTON_PIN)
-		if input_state == False:
+		input_state = not GPIO.input(BUTTON_PIN)
+		if input_state:
+			# print("Button pressed")
+			isPressed = True
+		elif isPressed:
+			isOn = not isOn
 			print("Button pressed")
-			time.sleep(0.2)
+			isPressed = False
+		time.sleep(0.2)
 			
 finally:
 	GPIO.cleanup()
