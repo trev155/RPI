cc4ad0eb802ea33b98247308d93f1a4666478b18
Trevor <trevku155@gmail.com>
2017-12-26 05:35:00 +0000

    Switches (#1)
    Multi-switch module.

---
+++ switch/multi_switch.py
@@ -0,0 +1,52 @@
+import RPi.GPIO as GPIO
+import time
+
+"""
+A module that has 2 input buttons. 
+When the left button is pressed, the left LEDs turn on.
+When the right button is pressed, the right LEDs turn on.
+When both buttons are pressed, the left, right, and middle LEDs turn on.
+"""
+BUTTON_LEFT=23
+BUTTON_RIGHT=18
+LED_LEFT=21
+LED_MID=16
+LED_RIGHT=12
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(BUTTON_LEFT, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(BUTTON_RIGHT, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(LED_LEFT, GPIO.OUT)
+GPIO.setup(LED_MID, GPIO.OUT)
+GPIO.setup(LED_RIGHT, GPIO.OUT)
+
+print("Module is ready - press a button!")
+
+try:
+	while True:
+		left_button = GPIO.input(BUTTON_LEFT)
+		right_button = GPIO.input(BUTTON_RIGHT)
+		if not left_button or not right_button:
+			if not left_button and not right_button:
+				GPIO.output(LED_LEFT, 1)
+				GPIO.output(LED_RIGHT, 1)
+				GPIO.output(LED_MID, 1)
+			elif not left_button:
+				GPIO.output(LED_LEFT, 1)
+				GPIO.output(LED_MID, 0)
+				GPIO.output(LED_RIGHT, 0)
+			elif not right_button:
+				GPIO.output(LED_RIGHT, 1)
+				GPIO.output(LED_LEFT, 0)
+				GPIO.output(LED_MID, 0)
+			else:
+				pass
+			time.sleep(0.2)
+		else:
+			GPIO.output(LED_LEFT, 0)
+			GPIO.output(LED_MID, 0)
+			GPIO.output(LED_RIGHT, 0)
+			time.sleep(0.2)
+	
+finally:
+	GPIO.cleanup()
