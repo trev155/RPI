096b93bad8c2a477dae9932d7297cf55a652690f
Trevor <trevku155@gmail.com>
2017-12-26 16:42:47 -0500

    code

--- rgb/rgb.py
+++ rgb/rgb.py
@@ -8,7 +8,7 @@ LED_LEFT=16
 LED_MID=20
 LED_RIGHT=21
 
-LED_MAPS = list(itertools.product((0, 1), repeat=3))
+LED_MAPS = [(0, 0, 0), (1, 0, 0), (0, 1, 0), (0, 0, 1), (1, 1, 0), (1, 0, 1), (0, 1, 1), (1, 1, 1)]
 LED_MAP_INDEX = 0
 LED_MAP = LED_MAPS[LED_MAP_INDEX]
 
@@ -28,14 +28,13 @@ try:
 			print("Button pressed")
 			# Change LED map config
 			LED_MAP_INDEX = LED_MAP_INDEX + 1
-			if LED_MAP_INDEX == len(LED_MAPS) - 1:
+			if LED_MAP_INDEX == len(LED_MAPS):
 				LED_MAP_INDEX = 0
 			LED_MAP = LED_MAPS[LED_MAP_INDEX]
-	
 		GPIO.output(LED_LEFT, LED_MAP[0])
 		GPIO.output(LED_MID, LED_MAP[1])
 		GPIO.output(LED_RIGHT, LED_MAP[2])
-		time.sleep(0.2)
+		time.sleep(0.3)
 finally:
 	GPIO.cleanup()
 
