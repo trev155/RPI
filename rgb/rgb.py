d644e09bdb811d82841e87f03f51a5e500dc8432
Trevor <trevku155@gmail.com>
2017-12-26 16:17:59 -0500

    rgb

--- rgb/rgb.py
+++ rgb/rgb.py
@@ -7,6 +7,8 @@ LED_LEFT=16
 LED_MID=20
 LED_RIGHT=21
 
+LED_MAP = [False, False, False]
+
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(BUTTON_LEFT, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 GPIO.setup(BUTTON_RIGHT, GPIO.IN, pull_up_down=GPIO.PUD_UP)
@@ -18,18 +20,15 @@ print("Module is ready - press a button!")
 
 try:
 	while True:
-		left_button = GPIO.input(BUTTON_LEFT)
-		right_button = GPIO.input(BUTTON_RIGHT)
-		if not left_button or not right_button:
+		# left_button = GPIO.input(BUTTON_LEFT)
+		right_button_pressed = not GPIO.input(BUTTON_RIGHT)
+		if right_button_pressed:
 			print("Button pressed")
-			if not left_button and not right_button:
-				GPIO.output(LED_LEFT, 0)
-				GPIO.output(LED_MID, 1)
-				GPIO.output(LED_RIGHT, 1)
+			# Change LED map config
 		else:
-			GPIO.output(LED_LEFT, 0)
-			GPIO.output(LED_MID, 0)
-			GPIO.output(LED_RIGHT, 0)
+			GPIO.output(LED_LEFT, LED_MAP[0])
+			GPIO.output(LED_MID, LED_MAP[1])
+			GPIO.output(LED_RIGHT, LED_MAP[2])
 		time.sleep(0.2)
 finally:
 	GPIO.cleanup()
