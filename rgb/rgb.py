a50852ef95167daebcd26e95666a1c71d78bfa4d
Trevor <trevku155@gmail.com>
2017-12-26 17:52:08 -0500

    c

--- rgb/rgb.py
+++ rgb/rgb.py
@@ -1,6 +1,5 @@
 import RPi.GPIO as GPIO
 import time
-import itertools
 
 BUTTON_LEFT=18
 BUTTON_RIGHT=23
@@ -49,7 +48,7 @@ try:
 			LED_MAP = LED_MAPS[LED_MAP_INDEX]
 			print("New LED map", LED_MAP)
 			rightIsPressed = False
-			
+
 		GPIO.output(LED_LEFT, LED_MAP[0])
 		GPIO.output(LED_MID, LED_MAP[1])
 		GPIO.output(LED_RIGHT, LED_MAP[2])
