cc65352604163799eb0395d19cb3786cdc2f2968
Trevor <trevku155@gmail.com>
2017-12-25 23:53:57 -0500

    multiple switches

--- switch/multi_switch.py
+++ switch/multi_switch.py
@@ -2,26 +2,32 @@ import RPi.GPIO as GPIO
 import time
 
 """
-A simple module that takes in a button press as input,
-and tells the LED to go on whenever the button is pressed.
+A module that has 2 input buttons. 
 """
 
-BUTTON_PIN=18
-LED_PIN=21
+BUTTON_LEFT=23
+BUTTON_RIGHT=18
+LED_LEFT=21
+LED_MID=16
+LED_RIGHT=12
 
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(BUTTON_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-GPIO.setup(LED_PIN, GPIO.OUT)
+GPIO.setup(BUTTON_LEFT, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(BUTTON_RIGHT, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(LED_LEFT, GPIO.OUT)
+GPIO.setup(LED_MID, GPIO.OUT)
+GPIO.setup(LED_RIGHT, GPIO.OUT)
 
 try:
 	while True:
-		input_state = GPIO.input(BUTTON_PIN)
-		if input_state == False:
+		left_button = GPIO.input(BUTTON_LEFT)
+		right_button = GPIO.input(BUTTON_RIGHT)
+		if left_button == False or right_button == False:
 			print("Button pressed")
-			GPIO.output(LED_PIN, 1)
+			GPIO.output(LED_LEFT, 1)
 			time.sleep(0.5)
 		else:
-			GPIO.output(LED_PIN, 0)
+			GPIO.output(LED_LEFT, 0)
 			
 finally:
 	GPIO.cleanup()
