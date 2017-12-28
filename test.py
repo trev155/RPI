3715d2ed07a53847c560e1a3949ac0999fddeef1
trevRPI <trevku155@gmail.com>
2017-12-27 19:58:15 -0500

    Setup for multi-LED patterns

--- test.py
+++ test.py
@@ -1,22 +1,34 @@
 import RPi.GPIO as GPIO
 import time
 
-# Test module
-BUTTON_PIN=23
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(BUTTON_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+LED_1 = 24
+LED_2 = 23
+LED_3 = 18
+LED_4 = 21
+LED_5 = 20
+LED_6 = 16
 
-isPressed = False
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(LED_1, GPIO.OUT)
+GPIO.setup(LED_2, GPIO.OUT)
+GPIO.setup(LED_3, GPIO.OUT)
+GPIO.setup(LED_4, GPIO.OUT)
+GPIO.setup(LED_5, GPIO.OUT)
+GPIO.setup(LED_6, GPIO.OUT)
 
 try:
-	while True:
-		input_state = not GPIO.input(BUTTON_PIN)
-		if input_state:
-			isPressed = True
-		elif isPressed:
-			print("Button pressed")
-			isPressed = False
-		time.sleep(0.2)
-			
+	GPIO.output(LED_1, 1)
+	GPIO.output(LED_2, 1)
+	GPIO.output(LED_3, 1)
+	GPIO.output(LED_4, 1)
+	GPIO.output(LED_5, 1)
+	GPIO.output(LED_6, 1)	
+	time.sleep(2)
+	GPIO.output(LED_1, 0)
+	GPIO.output(LED_2, 0)
+	GPIO.output(LED_3, 0)
+	GPIO.output(LED_4, 0)
+	GPIO.output(LED_5, 0)
+	GPIO.output(LED_6, 0)
 finally:
 	GPIO.cleanup()
